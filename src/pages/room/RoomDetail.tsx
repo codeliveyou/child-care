9c00c4f57307bbe817c475f98c5b37224d9ff499
codeliveyou <codetankstn@gmail.com>
2024-10-03 13:53:38 -0400

    changed checking part for message extend from guest

--- src/pages/room/RoomDetail.tsx
+++ src/pages/room/RoomDetail.tsx
@@ -273,7 +273,7 @@ const RoomPage: React.FC = () => {
     if (socketInstance) {
       socketInstance.on("room_message", (data: RoomMessage) => {
         const { from, message, to, timestamp } = data;
-        console.log('messagedata', data)
+        // console.log('messagedata', data)
         const newMessage: Message = {
           from: from === socketInstance.id ? "me" : from,
           to: to,
@@ -281,7 +281,7 @@ const RoomPage: React.FC = () => {
           timestamp,
           role: activeUser.role,
         };
-        if (from != myname)
+        if (to == "creator")
           setMessageList((prevList) => [...prevList, newMessage]);
       });
     }
